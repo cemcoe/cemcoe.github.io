c6a2e88caaf1831d2319138b4591044e7a60ddd5
chemxcode <chemxcode@gmail.com>
2019-04-08 18:05:38 +0800

    添加锚点菜单

--- nav/an/index.js
+++ nav/an/index.js
@@ -14,7 +14,7 @@ function alarmClock(message, alarmHour, stepSize) {
   }
 }
 
-alarmClock("碎觉觉", 22, 2);
+alarmClock("碎觉觉", 21, 2);
 
 
 //------------控制元素的显示和隐藏---------------
@@ -36,3 +36,60 @@ add.onclick = function () {
   }
 
 }
+
+
+
+// 弹出菜单
+var header = document.getElementsByTagName('header')[0];
+var headerAitem = header.getElementsByTagName('a')[0];
+var mainNav = document.getElementsByClassName('main-nav')[0];
+var rightFixed = document.getElementsByClassName('right-fixed')[0];
+
+rightFixed.onclick = function (e) {
+  // alert(1);
+  // 取消 a 的跳转事件
+  e.returnValue = false;
+  console.log("你点击了 right fixed ");
+  
+  if (mainNav.classList.contains('js-main-nav')) {
+    mainNav.classList.remove("js-main-nav");
+    console.log('js-main-nav 已添加');
+
+  } else {
+    mainNav.classList.add("js-main-nav");
+  }
+}
+
+
+mainNav.onclick = function () {
+  if (mainNav.classList.contains('js-main-nav')) {
+    mainNav.classList.remove("js-main-nav");
+    console.log('js-main-nav 已添加');
+    header.style.display = "none";
+    
+  }
+
+}
+
+
+var yourName;
+
+if (window.localStorage) {
+  console.log("浏览器支持localstorage");
+  // if (yourName == undefined) {
+  //   yourName = prompt("name", "");
+  // }
+  localStorage.Name = yourName;
+  if (localStorage.getItem('Name') == "undefined") {
+    // alert(localStorage.Name);
+    console.log(localStorage.Name);
+
+
+  }
+  console.log("localStorage.myName:" + localStorage.myName);
+
+} else {
+  alert("boom");
+}
+
+
