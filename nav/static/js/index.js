1584a479743eff8c2547cf6b73aec2415a4fd964
cemcoe <chemxcode@gmail.com>
2019-05-02 16:25:14 +0800

    add shengdan

--- nav/static/js/index.js
+++ nav/static/js/index.js
@@ -12,7 +12,7 @@ var headerA = header.getElementsByTagName('a')[0];
 function alarmClock(beginHour, endHour, message) {
   if (currentHours >= beginHour && currentHours <= endHour) {
     headerA.innerText = message;
-    
+
   }
 }
 
@@ -21,60 +21,35 @@ alarmClock(12, 13, "次饭饭");
 alarmClock(22, 23, "碎觉觉");
 
 
-//------------控制元素的显示和隐藏---------------
-var old = document.getElementsByClassName('old')[0];
-var add = document.getElementsByClassName('add')[0];
-var goWhere = document.getElementsByClassName('go-where')[0];
-
-var avopp = false;
-
-old.onclick = function () {
-  avopp = true;
-}
-
-add.onclick = function () {
-  if (avopp) {
-    alert('解锁成功，小的这就为你准备');
-    goWhere.style.display = "block";
-
-  }
-
-}
-//------------控制元素的显示和隐藏---------------
 
 
+// 滚动
+var giftHidden = document.getElementsByClassName("gift-hidden")[0];
+var loadings = document.getElementsByClassName('loadings')[0];
 
-// 弹出菜单
-var header = document.getElementsByTagName('header')[0];
-var headerAitem = header.getElementsByTagName('a')[0];
-var mainNav = document.getElementsByClassName('main-nav')[0];
-var rightFixed = document.getElementsByClassName('right-fixed')[0];
 
-rightFixed.onclick = function (e) {
-  // alert(1);
-  // 取消 a 的跳转事件
-  e.returnValue = false;
-  console.log("你点击了 right fixed ");
-  
-  if (mainNav.classList.contains('js-main-nav')) {
-    mainNav.classList.remove("js-main-nav");
-    console.log('js-main-nav 已添加');
+var demoTimeInterval = setInterval(function () {
+  if (getScrollOffset().y > headerA.offsetTop) {
+    headerA.style.position = "fixed";
+    loadings.style.position = "fixed";
+    giftHidden.style.display = "none";
+    console.log("圣诞老爷爷消失");
+
+    clearInterval(demoTimeInterval);
+    console.log("圣诞老爷爷消失，清除坏蛋定时器");
 
   } else {
-    mainNav.classList.add("js-main-nav");
-  }
-}
-
-
-mainNav.onclick = function () {
-  if (mainNav.classList.contains('js-main-nav')) {
-    mainNav.classList.remove("js-main-nav");
-    console.log('js-main-nav 已添加');
-    headerA.style.position = "static";
-    
+    console.log("待滚动");
   }
 
-}
+}, 1000);
+
+
+
+
+
+
+
 
 // localhost
 var yourName;
