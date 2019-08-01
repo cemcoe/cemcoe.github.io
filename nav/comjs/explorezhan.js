a23a1781847592bd596d136e3e580ea6b78e7b06
cemcoe <chemxcode@gmail.com>
2019-08-01 12:34:01 +0800

    update explore and footer

---
+++ nav/comjs/explorezhan.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: "#exploreZhan",
+    data: {
+        "exploreZhan":[{
+            id: "001",
+            url: "https://zh.wikipedia.org/",
+            title: "维基百科，自由的百科全书",
+            name: "wikipedia"
+
+        }, {
+            id: "002",
+            url: "https://acg.mn/",
+            title: "Mastodon（长毛象）社交网络",
+            name: "Mastodon（长毛象）社交网络"
+
+        }]
+    }
+
+
+})
---
+++ nav/comjs/siteInfo.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: "#footer",
+    data: {
+        update: "2019-7-27",
+        email: "admin@cemcoe.com",
+        copyright: "ChemCode"
+    }
+})
