40c3885217af9eb6b3ec66255d39aed280a04573
cemcoe <chemxcode@gmail.com>
2019-08-02 18:37:20 +0800

    update exlorezhang

--- nav/comjs/explorezhan.js
+++ nav/comjs/explorezhan.js
@@ -5,14 +5,40 @@ var app = new Vue({
             id: "001",
             url: "https://zh.wikipedia.org/",
             title: "维基百科，自由的百科全书",
-            name: "wikipedia"
+            name: "wikipedia",
+            icon: "https://zh.wikipedia.org/static/apple-touch/wikipedia.png"
 
         }, {
             id: "002",
             url: "https://acg.mn/",
             title: "Mastodon（长毛象）社交网络",
-            name: "Mastodon（长毛象）社交网络"
+            name: "Mastodon（长毛象）社交网络",
+            icon: "https://acg.mn/apple-touch-icon.png"
 
+        }, {
+            id: "003",
+            url: "https://javdb.com/",
+            title: "JavDB 日本成人影片資料庫",
+            name: "JavDB",
+            icon: "https://javdb.com/favicon-32x32.png",
+            keywords: "番号,种子,有码,无码,骑兵,步兵,福利,开车,老司机,日本成人电影,bt下载,磁力下载,gif出处,torrent,kitty,magnet"
+        }, {
+            id: "004",
+            url: "https://3cwork.com/",
+            title: "一早一晚 | 只工作，不上班",
+            name: "一早一晚",
+            icon: "https://3cwork.com/assets/favicon/favicon-32x32-6197ba182511d56654de3a1bdf935168f2714a89aad90644c5bb521f1852e1c9.png"
+        }, {
+            id: "005",
+            url: "https://www.slant.co/",
+            title: "Choose the best",
+            icon: "https://www.slant.co/images/favicon.png"
+        }, {
+            id: "006",
+            url: "https://lorefree.com/",
+            title: "LoreFree - 首个基于EOS和IPFS的去中心化知识共享社区",
+            icon: "https://lorefree.com/static/images/favicon-32x32.png"
+            
         }]
     }
 
--- nav/comjs/siteInfo.js
+++ nav/comjs/siteInfo.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: "#footer",
     data: {
-        update: "2019-7-27",
+        update: "2019-8-2",
         email: "admin@cemcoe.com",
         copyright: "ChemCode"
     }
