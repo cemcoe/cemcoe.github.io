cd4f145693bbb4813813fa76ab082ced48772edd
cemcoe <chemxcode@gmail.com>
2019-08-09 07:05:09 +0800

    update bookmarks

--- nav/comjs/siteInfo.js
+++ nav/comjs/siteInfo.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: "#footer",
     data: {
-        update: "2019-8-4",
+        update: "2019-8-9",
         email: "admin@cemcoe.com",
         copyright: "ChemCode"
     }
