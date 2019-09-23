63cea5d8e96c9c2fee7753f0a937a8d9670e33e0
cemcoe <chemxcode@gmail.com>
2019-09-23 14:38:58 +0800

    update nav

--- nav/comjs/siteInfo.js
+++ nav/comjs/siteInfo.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: "#footer",
     data: {
-        update: "2019-9-19",
+        update: "2019-9-23",
         email: "admin@cemcoe.com",
         copyright: "ChemCode"
     }
