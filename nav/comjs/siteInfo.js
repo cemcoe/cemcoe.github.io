895092e685c61064a7969f6ba35d730e30b665b5
cemcoe <chemxcode@gmail.com>
2019-09-27 18:10:01 +0800

    del nav front-end section

--- nav/comjs/siteInfo.js
+++ nav/comjs/siteInfo.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: "#footer",
     data: {
-        update: "2019-9-26",
+        update: "2019-9-27",
         email: "admin@cemcoe.com",
         copyright: "ChemCode"
     }
