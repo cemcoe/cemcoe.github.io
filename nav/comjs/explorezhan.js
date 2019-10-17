dde76f0ecd19a7ffba32ff89fa5f03308d50f798
cemcoe <chemxcode@gmail.com>
2019-10-17 15:45:46 +0800

    update nav list

--- nav/comjs/explorezhan.js
+++ nav/comjs/explorezhan.js
@@ -13,9 +13,9 @@ var app = new Vue({
             icon: "https://lh3.googleusercontent.com/BdBMEyMNvlK8QEd1FQg7GK_dfAw04WbllM390aBU1yLKOR0qjwqtJejVcV6-iKWgPdPJhTIM=w128-h128-e365"
         }, {
             id: "001",
-            url: "https://www.sigure.tw/",
-            title: "時雨の町 | 日文學習園地",
-            icon: "https://www.sigure.tw/templates/ol_transparent2/images/favicon.ico"
+            url: "https://www.xd0.com/",
+            title: "小刀娱乐网 | 网络那些事",
+            icon: "https://www.xd0.com/upload/1/146486/images/2015-7-3/20150703161516081608.png"
         }, {
             id: "001",
             url: "https://www.tofugu.com/",
@@ -31,11 +31,31 @@ var app = new Vue({
             url: "https://www.beautifulchemistry.net/",
             title: "Beautiful Chemistry",
             icon: "https://images.squarespace-cdn.com/content/55520a32e4b08eb084d46a8d/1433170542458-CVL4HV2QJ5YHYYLINWYR/BC_fav.png?format=1000w&content-type=image%2Fpng"
+        },  {
+            id: "002",
+            url: "https://abema.tv/",
+            title: "AbemaTV｜国内最大の無料インターネットテレビ局",
+            icon: "https://hayabusa.io/abema/assets/img_icon.w180.h180.v1f486dd.png"
         }, {
             id: "003",
             url: "https://go.cemcoe.workers.dev/",
-            title: "Netflav | 讓您在線上觀賞日本成人電影，有福利",
+            title: "go.cemcoe.workers",
             icon: "https://netflav.com/static/favicon.ico"
+        }, {
+            id: "001",
+            url: "https://www.wuzuowei.net",
+            title: "无作为 | 分享不仅仅是一种态度！",
+            icon: "https://www.wuzuowei.net/favicon.ico"
+        }, {
+            id: "001",
+            url: "https://m.xiachufang.com/",
+            title: "下厨房 | 吃饭那些事",
+            icon: "https://s.chuimg.com/favicon.ico"
+        }, {
+            id: "001",
+            url: "https://www.sigure.tw/",
+            title: "時雨の町 | 日文學習園地",
+            icon: "https://www.sigure.tw/templates/ol_transparent2/images/favicon.ico"
         }]
 
     }
