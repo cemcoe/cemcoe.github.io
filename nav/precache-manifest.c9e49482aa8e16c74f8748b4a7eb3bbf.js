7d0a44e59e9f24aac714851647335986722e9189
cemcoe <chemxcode@gmail.com>
2020-05-05 11:24:33 +0800

    update nav vue version

---
+++ nav/precache-manifest.c9e49482aa8e16c74f8748b4a7eb3bbf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ecb2f0759e959b8fb1",
+    "url": "/nav/css/chunk-eb6e17c2.c7cf8922.css"
+  },
+  {
+    "revision": "48732a635ca1a2a08910",
+    "url": "/nav/css/chunk-vendors.a4280950.css"
+  },
+  {
+    "revision": "980205862659f7cc9c78f0d5b6e670c1",
+    "url": "/nav/img/loading.98020586.webp"
+  },
+  {
+    "revision": "14b0ef16b1497fab30efadd682ccc1c1",
+    "url": "/nav/index.html"
+  },
+  {
+    "revision": "5bab4418e2054f1a9acc",
+    "url": "/nav/js/about.fbe4c988.js"
+  },
+  {
+    "revision": "f9019ed1aed3ad7dcde5",
+    "url": "/nav/js/app.ad9240df.js"
+  },
+  {
+    "revision": "a3ecb2f0759e959b8fb1",
+    "url": "/nav/js/chunk-eb6e17c2.94a15bef.js"
+  },
+  {
+    "revision": "48732a635ca1a2a08910",
+    "url": "/nav/js/chunk-vendors.feb97480.js"
+  },
+  {
+    "revision": "eeb6fe12542436f9e90b33d9169a9706",
+    "url": "/nav/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nav/robots.txt"
+  }
+]);
